e501dc42db79cc20df2203888b5ede2d3088aef7
Najmo Aden <najmoaden138@gmail.com>
2025-07-03 17:03:17 +0300

    :star: feat: the bind method

--- script.js
+++ script.js
@@ -169,3 +169,50 @@ book.apply(swiss, flightData);
 console.log(swiss);
 
 book.call(swiss, ...flightData);
+
+////////////////////////////////////////////////
+// The bind Method
+//book.call(eurowings, 23, 'Sarah Williams');
+
+const bookEW = book.bind(eurowings);
+const bookLH = book.bind(luftansa);
+const bookLX = book.bind(swiss);
+
+bookEW(23, 'Steven Williams');
+
+const bookEW23 = book.bind(eurowings, 23);
+bookEW23('Jonas Scmedtmann');
+bookEW23('Martha Cooper');
+
+// With Event Listeners
+luftansa.planes = 300;
+luftansa.buyPlane = function () {
+  console.log(this);
+  this.planes++;
+  console.log(this.planes);
+};
+
+//luftansa.buyPlane();
+
+document
+  .querySelector('.buy')
+  .addEventListener('click', luftansa.buyPlane.bind(luftansa));
+
+// Partial application
+const addTax = (rate, value) => value + value * rate;
+console.log(addTax(0.1, 200));
+
+const addVAT = addTax.bind(null, 0.23);
+//addVAT = value => value + value *0.23;
+console.log(addVAT(100));
+console.log(addVAT(23));
+
+const addTaxRate = function (rate) {
+  return function (value) {
+    return value + value * rate;
+  };
+};
+
+const addVAT2 = addTaxRate(0.23);
+console.log(addVAT2(100));
+console.log(addVAT2(23));
